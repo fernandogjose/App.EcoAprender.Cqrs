e73355ff2abdef7fa76dde2ef628a55681ab9116
Fernando <fernandogjose@gmail.com>
2020-05-04 22:15:08 -0300

    inclusão do swagger

--- Src/App.EcoAprender.Cqrs.Api/Startup.cs
+++ Src/App.EcoAprender.Cqrs.Api/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace App.EcoAprender.Cqrs.Api
 {
@@ -22,6 +24,22 @@ namespace App.EcoAprender.Cqrs.Api
 
             // IoC
             BootStrapper.RegisterServices(services);
+
+            // Swagger
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Kafka Sender",
+                        Version = "v1",
+                        Description = "Exemplo de API REST criada com o ASP.NET Core 3.0 para enviar mensagens ao Kafka",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Fernando José",
+                            Url = new Uri("https://github.com/fernandogjose")
+                        }
+                    });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -30,6 +48,12 @@ namespace App.EcoAprender.Cqrs.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Ativando middlewares para uso do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Kafka Sender V1");
+            });
 
             //app.UseHttpsRedirection();
             app.UseRouting();
